91a899af2fd8553d067aad4f561883c6b7373572
Vivek Kairi <vivekkairi30oct@gmail.com>
2019-09-24 23:21:11 +0530

    Added test upload

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask,request, render_template, flash, redirect, url_for,session, logging
 from flask_mysqldb import MySQL 
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators, DateTimeField, BooleanField, IntegerField
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 from passlib.hash import sha256_crypt
 from functools import wraps
+from werkzeug.utils import secure_filename
+from docx import Document
+import json
+from coolname import generate_slug
 
 
 app = Flask(__name__)
 app.secret_key= 'hui'
 
-#C onfig MySQL
+#Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'nubaf'
 app.config['MYSQL_PASSWORD'] = 'nubafgg'
@@ -29,6 +35,20 @@ def is_logged(f):
 
 	return wrap
 
+def doctodict(filepath):
+    document = Document(filepath)
+    data={}
+    count=1
+    for table in document.tables:
+        temp = {}
+        for rowNo,row in enumerate(table.rows):
+            temp[table.cell(rowNo, 0).text]=table.cell(rowNo, 1).text
+        data[count] = temp
+        count+=1
+ 
+    return data
+ 
+
 
 class RegisterForm(Form):
 	name = StringField('Name', [validators.Length(min=3, max=50)])
@@ -41,9 +61,18 @@ class RegisterForm(Form):
 	confirm = PasswordField('Confirm Password')
 
 
+class UploadForm(FlaskForm):
+    doc = FileField('Docx Upload', validators=[FileRequired()])
+    start_date_time = DateTimeField('Start Date & Time')
+    end_date_time = DateTimeField('End Date & Time')
+    show_result = BooleanField('Show Result after completion')
+    duration = IntegerField('Duration')
+    password = StringField('Test Password', [validators.Length(min=3, max=6)])
+
+
 @app.route('/')
 def index():
-	return 'Index'
+	return render_template('layout.html')
 
 
 @app.route('/register', methods=['GET','POST'])
@@ -93,9 +122,43 @@ def dashboard():
 	return render_template('dashboard.html')
 
 
-
 @app.route('/logout')
 def logout():
 	session.clear()
 	flash('Successfully logged out', 'success')
 	return redirect(url_for('index'))
+
+
+@app.route('/create-test', methods = ['GET', 'POST'])
+@is_logged
+def create_test():
+	form = UploadForm()
+	if request.method == 'POST' and form.validate_on_submit():
+		f = form.doc.data
+		filename = secure_filename(f.filename)
+		f.save('questions/' + filename)
+		cur = mysql.connection.cursor()
+		d = doctodict('questions/' + f.filename.replace(' ', '_').replace('(','').replace(')',''))
+		test_id = generate_slug(2)
+		for no, data in d.items():
+			marks = data['((MARKS)) (1/2/3...)']
+			a = data['((OPTION_A))']
+			b = data['((OPTION_B))']
+			c = data['((OPTION_C))']
+			d = data['((OPTION_D))']
+			question = data['((QUESTION))']
+			correct_ans = data['((CORRECT_CHOICE)) (A/B/C/D)']
+			explanation = data['((EXPLANATION)) (OPTIONAL)']
+			cur.execute('INSERT INTO questions(test_id,qid,q,a,b,c,d,ans,marks) values(%s,%s,%s,%s,%s,%s,%s,%s,%s)', 
+				(test_id,no,question,a,b,c,d,correct_ans,marks))
+			mysql.connection.commit()
+		start_date_time = form.start_date_time.data
+		end_date_time = form.end_date_time.data
+		show_result = form.show_result.data
+		duration = form.duration.data
+		password = form.password.data
+		cur.execute('INSERT INTO teachers (username, test_id, start, end, duration, show_ans, password) values(%s,%s,%s,%s,%s,%s,%s)',
+			(dict(session)['username'], test_id, start_date_time, end_date_time, duration, show_result, password))
+		mysql.connection.commit()
+		cur.close()
+	return render_template('create_test.html' , form = form)
